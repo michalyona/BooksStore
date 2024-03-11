dbddd4403f5b8096a898d049ce80f9c407b92537
michal <my0527615247@gmail.com>
2024-03-11 20:56:03 +0200

    Add project files.

---
+++ BooksStoreBL/BookStoreBL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksStoreDAL;
+using Entities;
+
+namespace BooksStoreBL
+{
+    public class BookStoreBL
+    {
+        List<BookDetails> ListOfBooks;
+        FileConnection fileConnection;
+        public BookStoreBL()
+        {
+            fileConnection = new FileConnection();
+            ListOfBooks = fileConnection.ReadAllBooks();
+        }
+
+        public IEnumerable<BookDetails> GetAllBooks()
+        {
+            return ListOfBooks;
+        }
+        public IEnumerable<BookDetails> a()
+        {
+            return ListOfBooks.Where(BookDetails=>BookDetails.Price > 30);
+        }
+        public IEnumerable<BookDetails> b()
+        {
+            return ListOfBooks.OrderBy(BookDetails => BookDetails.Id);
+        }
+        public IEnumerable<BookDetails> c()
+        {
+            var result= ListOfBooks.Where(BookDetails => BookDetails.IsComics=true);
+            return (IEnumerable<BookDetails>)result.Select(BookDetails => BookDetails.Price);
+        }
+        public IEnumerable<BookDetails> d()
+        {
+            var result = ListOfBooks.Where(for9);
+            return (IEnumerable<BookDetails>)result.Select(BookDetails => BookDetails.Name);
+        }
+
+        private bool for9(BookDetails BookDetails)
+        {
+            if(BookDetails.MaxAge>9&& BookDetails.MinAge<9)
+              return true; 
+            return false;
+        }
+    }
+    
+}
---
+++ BooksStoreGUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BooksStoreGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BooksStoreBL.BookStoreBL booksStoreBL; 
+        public MainWindow()
+        {
+            booksStoreBL = new BooksStoreBL.BookStoreBL();
+            
+            InitializeComponent();
+            var res = booksStoreBL.GetAllBooks();
+            BooksDataGrid.ItemsSource = res;
+            var res2 = booksStoreBL.a();
+            BooksDataGrid.ItemsSource = res2;
+            var res3 = booksStoreBL.b();
+            BooksDataGrid.ItemsSource = res3;
+            var res4 = booksStoreBL.c();
+            BooksDataGrid.ItemsSource = res4;
+            var res5 = booksStoreBL.d();
+            BooksDataGrid.ItemsSource = res5;
+
+        }
+    }
+}
